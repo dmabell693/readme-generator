8b8642187c8969bda34fd281fd265953e3e92f8a
Daniel Abell <da450@cairn.edu>
2020-04-10 22:54:19 -0400

    created base functionality. when index.js is run in the command line, user is prompted with a series of questions whose answers are taken and used to populate the content of a readme.md file.

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-function writeToFile(fileName, data) {
+// const questions = [
+
+// ];
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is your application's name?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "What exactly can your project do?"
+        },
+        {
+            type: "input",
+            name: "toc",
+            message: "What will be your README headings?"
+        }
+    ]);
 }
 
-function init() {
+function generateMarkdown(data) {
+    return `
+  # ${data.title}
 
+  ## ${data.description}
+
+  ## ${data.toc}
+  
+  `;
+  }
+
+// function writeToFile(fileName, data) {
+// }
+
+async function init() {
+    console.log("hi");
+    try {
+        const data = await promptUser();
+        
+        const markdown = generateMarkdown(data);
+
+        await writeFileAsync("README2.md", markdown);
+
+        console.log("Successfully wrote to README2.md");
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 init();
